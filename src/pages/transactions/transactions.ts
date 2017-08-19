9365f174b0e9ce1d95a8f3fdc81c55acfbc5ee3d
unknown <enkee03@hotmail.com>
2017-08-19 00:00:48 -0500

    Movimientos 1

---
+++ src/pages/transactions/transactions.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import {Transaction} from '../../database';
+import {AddingPage} from '../adding/adding';
+/**
+ * Generated class for the TransactionsPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-transactions',
+  templateUrl: 'transactions.html',
+})
+export class TransactionsPage {
+
+  title : string = "Movimientos";
+  transactions : any;
+  addingPage = AddingPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    let transaction = new Transaction(20,"Primera Transaccion");
+
+    transaction.save();
+    this.loadTransactions();
+  }
+
+  loadTransactions(){
+    Transaction.all()
+               .then((resultados) => {
+                 this.transactions = resultados
+                 console.log(this.transactions);
+                });
+  }
+
+}
